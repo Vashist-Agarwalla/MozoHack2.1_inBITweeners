853be15f7ef8c2689785aa3a73526a0e0f826e58
Vashist-Agarwalla <62013535+Vashist-Agarwalla@users.noreply.github.com>
2021-03-20 16:01:49 +0530

    Update

---
+++ models/plant.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const plantSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    species: {
+        type: String,
+        require: true
+    },
+    climate: {
+        type: [String]
+    },
+    water: {
+        type: String,
+    },
+    soil: {
+        type: String,
+    },
+    pottingMix: {
+        type: String,
+    },
+    sunlight: {
+        type: String,
+    },
+    pottedBeds: {
+        type: String,
+    },
+    growthTime: {
+        type: String,
+    },
+    rooftop: {
+        type: Boolean,
+    },
+    article: {
+        type: String,
+    },
+    youtube: {
+        type: String,
+    },
+    image: {
+        type: String,
+    }
+})
+
+const PlantData = mongoose.model('PlantData', plantSchema)
+
+module.exports = PlantData;
