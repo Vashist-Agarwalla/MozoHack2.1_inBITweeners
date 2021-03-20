2670ae405f0cc6022569f37ac79f03eb22466276
Vashist-Agarwalla <62013535+Vashist-Agarwalla@users.noreply.github.com>
2021-03-20 09:39:13 +0530

    userSchema

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    location: {
+        type: String
+    }
+})
+
+const UserData = mongoose.model('UserData', userSchema)
+
+module.exports = UserData;
